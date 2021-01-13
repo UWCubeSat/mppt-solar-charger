a0cf4b95111ce3f2b09b7f95a5c30dfa80038a96
Katharine Lundblad <klundblad@live.com>
2021-01-12 21:32:06 -0800

    Add files via upload

---
+++ arduino/Scan_I2C_Address.ino
@@ -0,0 +1,47 @@
+/**
+ * Returning I2C Addresses from AD5245
+ * Source: https://randomnerdtutorials.com
+ * 
+ * Created by Katharine Lundblad on January 9, 2021
+ */
+
+#include <Wire.h>
+ 
+void setup() {
+  Wire.begin();
+  Serial.begin(115200);
+  Serial.println("\nI2C Scanner");
+}
+ 
+void loop() {
+  byte error, address;
+  int nDevices;
+  Serial.println("Scanning...");
+  nDevices = 0;
+  for (address = 1; address < 127; address++ ) {
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+    if (error == 0) {
+      Serial.print("I2C device found at address 0x");
+      if (address<16) {
+        Serial.print("0");
+      }
+      Serial.println(address,HEX);
+      nDevices++;
+    } else if (error == 4) {
+      Serial.print("Unknow error at address 0x");
+      if (address<16) {
+        Serial.print("0");
+      }
+      Serial.println(address,HEX);
+    }    
+  }
+  
+  if (nDevices == 0) {
+    Serial.println("No I2C devices found\n");
+  } else {
+    Serial.println("done\n");
+  }
+  
+  delay(5000);      
+}
